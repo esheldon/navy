d7525cf08cf6c9febb7e4669f1b99a063c967c31
Erin Sheldon <erin.sheldon@gmail.com>
2017-08-17 08:32:27 -0400

    bump version

--- navy/__init__.py
+++ navy/__init__.py
@@ -7,3 +7,5 @@ from .thenavy import (
 )
 
 from . import comm
+
+__version__="v0.9.0"
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="navy", 
     packages=['navy'],
-    version="0.1",
+    version="0.9.0",
 )
 
 
